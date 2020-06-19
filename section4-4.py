3b7b3e401496798ebe651abbdcbaaa7338f58fde
dragon <dydrb141@naver.com>
2020-06-19 13:30:48 +0900

    - 파이썬 4-4번째 예제

--- section4-4.py
+++ section4-4.py
@@ -37,6 +37,48 @@ print(2 in b)
 print('name' in  a)
 print('name2' not in a)
 
+#집합 (Sets) (순서x, 중복x)
+a = set()
+b = set([1, 2,3, 4])
+c= set([1, 4, 5, 6, 6])
+
+print(type(a))
+print(c)
+
+t = tuple(c)
+print(t)
+
+l = list(c)
+print(l)
+
+s1 = set([1,2,3,4,5,6,])
+s2 = set([4,5,6,7,8,9,])
+
+print(s1.intersection(s2)) #교집합
+print(s1 & s2) #위와 동일
+
+print(s1.union(s2)) #합집합
+print(s1 | s2)  #위와 동일
+
+print(s1 - s2) #차집합
+print(s1.difference(s2))
+
+# 추가 & 제거
+s3 = set([7,8,10,15])
+
+s3.add(18)
+s3.add(7)
+
+s3.remove(15)
+
+print(s3)
+
+
+
+
+
+
+
 
 
 
